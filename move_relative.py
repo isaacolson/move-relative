15559d5067a7d96ae218208d1404b64882113ea3
isaacolson <olsonites@gmail.com>
2018-10-10 18:27:17 -0500

    Update move_relative.py

--- move_relative.py
+++ move_relative.py
@@ -9,7 +9,7 @@ pos_x = device.get_current_position('x')
 pos_y = device.get_current_position('y')
 pos_z = z_height
 
-device.move_relitive(x_dist, y_dist, z_dist, 100)
+device.move_relative(x_dist, y_dist, z_dist, 100)
 
 device.log('ending!', 'success', ['toast'])
 
