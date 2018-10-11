70d048820f78e0ca8da9b5808d6ee6ffe523b794
isaacolson <olsonites@gmail.com>
2018-10-11 14:31:12 -0500

    Update move_relative.py

--- move_relative.py
+++ move_relative.py
@@ -4,6 +4,10 @@
 from farmware_tools import device
 from farmware_tools import get_config_value
 
+x_dist = get_config_value('Move Relative', 'rel_x')
+y_dist = get_config_value('Move Relative', 'rel_y')
+z_dist = get_config_value('Move Relative', 'rel_z')
+
 device.log('starting! ' + str(x_dist), 'success', ['toast'])
 
 device.move_relative(x_dist, 10, -10, 50)
@@ -14,6 +18,4 @@ device.log('ending!', 'success', ['toast'])
 if __name__ == '__main__':
     farmware_name = 'move_relative'
     # Load inputs from Farmware page widget specified in manifest file
-    x_dist = get_config_value('Move Relative', 'rel_x')
-    y_dist = get_config_value('Move Relative', 'rel_y')
-    z_dist = get_config_value('Move Relative', 'rel_z')
+
